59afbbcd18c2441b944573a8b6931591d645115d
Gautam-S-N <gautamsnarajji@gmail.com>
2026-05-02 15:06:38 +0530

    gsn

--- backend/scripts/check_sales.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function run() {
-  const s = await prisma.sale.findMany({ include: { createdBy: true } });
-  console.log(s.map(x => ({ amt: x.totalAmount, user: x.createdBy.firstName })));
-}
-run().catch(console.error).finally(() => prisma.$disconnect());
